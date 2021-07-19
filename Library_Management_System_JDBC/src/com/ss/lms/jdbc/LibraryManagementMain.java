d09a61f72b52154aa4a834b3bd4c5311793cc016
Dhrubodh <dhrubo.talukder@smoothstack.com>
2021-07-19 02:39:30 -0400

    Started with the lms project. created a one line welcome menu

---
+++ Library_Management_System_JDBC/src/com/ss/lms/jdbc/LibraryManagementMain.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.ss.lms.jdbc;
+
+import com.ss.lms.menu.WelcomeMenu;
+
+/**
+ * @author dhrubo
+ *
+ */
+public class LibraryManagementMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Entry point for the app.
+		// Will trigger the welcome menu
+		WelcomeMenu wm = new WelcomeMenu();
+		wm.welcome();
+	}
+
+}
---
+++ Library_Management_System_JDBC/src/com/ss/lms/menu/WelcomeMenu.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.ss.lms.menu;
+
+/**
+ * @author dhrubo
+ *
+ */
+public class WelcomeMenu {
+	
+	public void welcome() {
+		System.out.println("Welcome to Smoothstack Library!");
+	}
+
+}
